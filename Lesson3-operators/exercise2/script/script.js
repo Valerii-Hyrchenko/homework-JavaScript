21f1829243f412f2d282053c96ac2d8b6e72c63c
Valerii Hyrchenko <girchenko2013@gmail.com>
2022-06-20 21:39:16 +0300

    fixed 2 and 3 exercises in lesson3_operators

--- Lesson3-operators/exercise2/script/script.js
+++ Lesson3-operators/exercise2/script/script.js
@@ -1,34 +1,22 @@
 let valueA = Number (prompt ("Enter number A!"));
 let valueB = Number (prompt ("Enter number B!"));
 let typeOperation = prompt ("Please enter the type of operation. (+), (-), (/), (*)");
-function resultSum(valueA,valueB) {
-    return valueA + valueB;
-}
-function resultSubtraction(valueA,valueB) {
-    return valueA - valueB;
-}
-function resultDivide(valueA,valueB) {
-    return valueA / valueB;
-}
-function resultMultiply(valueA,valueB) {
-    return valueA * valueB;
-}
 switch (typeOperation) {
     case "+":
-        alert (`Your result is ${resultSum(valueA,valueB)}`);
+        (isNaN(valueA) == false) && (isNaN(valueB) == false) ? alert (`Your result is ${valueA + valueB}!`) : alert ("Please enter numbers correctly!");
         break;
     case "-":
-        alert (`Your result is ${resultSubtraction(valueA,valueB)}`);
+        (isNaN(valueA) == false) && (isNaN(valueB) == false) ? alert (`Your result is ${valueA - valueB}!`) : alert ("Please enter numbers correctly!");
         break;
     case "/":
         if (valueB != 0) {
-            alert (`Your result is ${resultDivide(valueA,valueB)}`);
+            (isNaN(valueA) == false) && (isNaN(valueB) == false) ? alert (`Your result is ${valueA / valueB}!`) : alert ("Please enter numbers correctly!");
         } else {
-            alert ("Cannot be divided by zero!");
+            alert ("Cannot be divided by null!");
         }
         break;
     case "*":
-        alert (`Your result is ${resultMultiply(valueA,valueB)}`);
+        (isNaN(valueA) == false) && (isNaN(valueB) == false) ? alert (`Your result is ${valueA * valueB}!`) : alert ("Please enter numbers correctly!");
         break;
     default:
         alert ("You entered the wrong type of operation. Please enter it correctly.");
--- Lesson3-operators/exercise3/script/script.js
+++ Lesson3-operators/exercise3/script/script.js
@@ -1,9 +1,17 @@
 let visitorAge = Number(prompt ("Hello! Please enter your age!"));
+let parentalPermission;
 if (visitorAge <= 80 && visitorAge >= 12) {
     if (visitorAge < 60 && visitorAge > 18) {
         alert ("Welcome to the amusement park!");
     } else {
-        alert ("You can visit the amusement park only with the permission of your parents or other responsible person!");
+        parentalPermission = prompt ("Do you have parental permission to visit the amusement park? Enter yes/no.");
+        if (parentalPermission == "yes") {
+            alert ("Welcome to the amusement park!");
+        } else if (parentalPermission == "no") {
+            alert ("You are not allowed to visit the amusement park!");
+        } else {
+            alert ("Please enter your answer correctly!");
+        }
     }
 } else {
     alert ("You are not allowed to visit the amusement park!");

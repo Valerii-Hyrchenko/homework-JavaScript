4ff2c5423f32103fc6d62c14dfc4628b5c45d32a
Valerii Hyrchenko <girchenko2013@gmail.com>
2022-07-05 15:16:02 +0300

    Fixed Lesson6 and add dir Lesson7_Objects

--- Lesson6-numbers,strings/script.js
+++ Lesson6-numbers,strings/script.js
@@ -1,25 +1,15 @@
 //Task 1
-const checkValidRegistration = () => {
+const checkValidName = () => {
     let userName;
     let userSurname;
-    let userPassword;
-    let isValidSuccess = false;
-    while (!isValidSuccess){
-        userName = prompt("Please enter your name!").trim();
-        userSurname = prompt("Please enter your surname!").trim();
+    let isValidNameSuccess = false;
+    while (!isValidNameSuccess){
+        userName = prompt("Please enter your name!");
+        userSurname = prompt("Please enter your surname!");
         if (userName === null || userSurname === null) {
             alert("You didn't enter your name. Try again!");
         } else if (userName.length > 1 && userSurname.length > 1) {
-            alert("Your password must consist of at least 6 characters and contain uppercase letters.");
-            userPassword = prompt("Please enter your password!").trim();
-            for (let i = 0; i < userPassword.length; i++) {
-                let countSlice = i + 1;
-                if (userPassword.slice(i, countSlice) === userPassword.slice(i, countSlice).toUpperCase() && userPassword.length > 6) {
-                    isValidSuccess = true;
-                } else if (countSlice === userPassword.length && isValidSuccess === false) {
-                    alert ("You enter wrong password!");
-                }
-            }
+            isValidNameSuccess = true;
         } else {
             alert ("You have entered a short name, it must be longer than 1 letter. Try again!");
         }
@@ -28,7 +18,23 @@ const checkValidRegistration = () => {
     alert(`User name is ${userName[0].toUpperCase() + userName.slice(1).toLowerCase()} and surname is ${userSurname[0].toUpperCase() + userSurname.slice(1).toLowerCase()}`);
     return true;
 }
-console.log(`checkValidRegistration`, checkValidRegistration());
+const checkValidPassword = () => {
+    let userPassword;
+    let isValidPasswordSuccess = false;
+    while (!isValidPasswordSuccess) {
+        alert("Your password must consist of at least 6 characters and contain uppercase letters.");
+        userPassword = prompt("Please enter your password!").trim();
+        if (userPassword.toLowerCase() === userPassword || userPassword.toUpperCase() === userPassword || userPassword.length < 6) {
+            alert ("You enter wrong password!");
+        } else {
+            isValidPasswordSuccess = true;
+        }
+    }
+    alert("Validation success!");
+    return true;
+    }
+//console.log(`checkValidName`, checkValidName());
+console.log(`checkValidPassword`, checkValidPassword());
 //Task 2
 const getRandomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min)) + min;
